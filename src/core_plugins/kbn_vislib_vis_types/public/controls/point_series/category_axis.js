9b5d3cfeec284613467def19cf50ca7ebd3fa554
Brian Frantz <gamercubed@brianfrantz.com>
2018-06-17 20:56:50 -0500

    Update category_axis.js
    Fixed merge oversights

--- src/core_plugins/kbn_vislib_vis_types/public/controls/point_series/category_axis.js
+++ src/core_plugins/kbn_vislib_vis_types/public/controls/point_series/category_axis.js
@@ -1,4 +1,3 @@
-
 /*
  * Licensed to Elasticsearch B.V. under one or more contributor
  * license agreements. See the NOTICE file distributed with
@@ -7,7 +6,7 @@
  * the Apache License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License.
  * You may obtain a copy of the License at
-  
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
